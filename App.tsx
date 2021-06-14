0fe8cd61835ce8e32ead14ef611ef0d588990817
Jonathan Dahan <git@jonathan.is>
2021-06-14 18:31:56 -0400

    d3-scale faile

--- App.tsx
+++ App.tsx
@@ -1,11 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { scaleTime } from 'd3-scale';
 
 export default function App() {
+  const scaleX = scaleTime().domain([0, 100]).range([0, 50])
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>{scaleX}</Text>
       <StatusBar style="auto" />
     </View>
   );
